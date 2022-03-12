496460e0979f545d68c43074a319df729048bafe
vallin1991 <49724891+vallin1991@users.noreply.github.com>
2022-03-12 07:21:56 -0800

    Add files via upload
    Transactions.sol for Ethereum blockchain transactions, also we created the deploy.js for deploying the contract and the hardhat.config.js got configured for the testing of the smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
+const { defaultAccounts } = require('ethereum-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+require('@nomiclabs/hardhat-waffle');
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.0',
+  networks: {
+    ropsten:{
+      url: 'https://eth-ropsten.alchemyapi.io/v2/fEoAIku2M2w9J9li6_-Wc198a36S-tuX',
+      accounts: ['379de39ea4b77e48922f6012330a1737a9ae07c814b5cee4a5f4b19887027ba8']
+    }
+  }
+}
